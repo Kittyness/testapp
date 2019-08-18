42ae6a0938258a910fa4a07a41ddaeb78555efc2
Kate Nesmyelova <kate.nesmyelova@vendhq.com>
2019-08-18 23:40:54 +1200

    Added some tweaks

--- cypress/integration/simple-sale.ts
+++ cypress/integration/simple-sale.ts
@@ -2,63 +2,65 @@
 
 import * as product from "../support/product";
 
-it("Adding a product to a sale", function() {
-  cy.visit("/signin", { failOnStatusCode: false });
-  cy.get("#signin_username")
-    .should("be.visible")
-    .type(Cypress.env("cashierUser"));
+describe("Simple Test for a Cashier", function() {
+  it("Adding a product to a sale", function() {
+    cy.visit("/signin", { failOnStatusCode: false });
+    cy.get("#signin_username")
+      .should("be.visible")
+      .type("Cashier");
 
-  cy.get("#signin_password").type(Cypress.env("cashierPassword"));
+    cy.get("#signin_password").type("testCashier");
 
-  cy.get('[name="signin_submit"]').click();
+    cy.get('[name="signin_submit"]').click();
 
-  cy.get(".nv-topnav-content .vd-nav-item-label")
-    .contains(Cypress.env("cashierUser"))
-    .should("be.visible");
+    cy.get(".nv-topnav-content .vd-nav-item-label")
+      .contains("Cashier")
+      .should("be.visible");
 
-  const currentProduct: product.ProductInfo = {
-    name: "Chocolate Brown",
-    price: "3.50",
-    taxRate: 15,
-    taxLabel: "GST"
-  };
+    const currentProduct: product.ProductInfo = {
+      name: "Chocolate Brown",
+      price: "3.50",
+      taxRate: 15,
+      taxLabel: "GST"
+    };
 
-  const taxValue = "0.46";
+    const taxValue = "0.46";
 
-  cy.get(".wr-global-search vd-autocomplete .vd-autocomplete-input").type(
-    currentProduct.name
-  );
+    cy.get(".wr-global-search vd-autocomplete .vd-autocomplete-input").type(
+      currentProduct.name
+    );
 
-  // Select the first product from suggestions
-  cy.get("vd-suggestion")
-    .eq(0)
-    .parent()
-    .click();
+    // Select the first product from suggestions
+    cy.get("vd-suggestion")
+      .eq(0)
+      .parent()
+      .click();
 
-  cy.get(".sale-list > .sale-list-item")
-    .eq(0)
-    .should("contain", currentProduct.price);
+    cy.get(".sale-list > .sale-list-item")
+      .eq(0)
+      .should("contain", currentProduct.price);
 
-  cy.get(".sale-total-line.sale-add-line.pro-total-line-total-tax")
-    .should("contain", "Tax")
-    .should("contain", currentProduct.taxLabel)
-    .should("contain", taxValue);
+    cy.get(".sale-total-line.sale-add-line.pro-total-line-total-tax")
+      .should("contain", "Tax")
+      .should("contain", currentProduct.taxLabel)
+      .should("contain", taxValue);
 
-  cy.get(".sale-footer .btn--pay .label-and-items-container")
-    .should("contain", "Pay")
-    .should("contain", "1 item");
+    cy.get(".sale-footer .btn--pay .label-and-items-container")
+      .should("contain", "Pay")
+      .should("contain", "1 item");
 
-  cy.get(".sale-footer .btn--pay .pay-action-value").should(
-    "contain",
-    `$${currentProduct.price}`
-  );
+    cy.get(".sale-footer .btn--pay .pay-action-value").should(
+      "contain",
+      `$${currentProduct.price}`
+    );
 
-  // Pay for Sale
-  cy.get(".pro-sale-ready .btn--pay").click();
+    // Pay for Sale
+    cy.get(".pro-sale-ready .btn--pay").click();
 
-  cy.contains(".pro-payments-ready .btn--payment-type", "Cash").click();
+    cy.contains(".pro-payments-ready .btn--payment-type", "Cash").click();
 
-  cy.get(".btn--complete-sale")
-    .contains("Done (ESC)")
-    .click();
+    cy.get(".btn--complete-sale")
+      .contains("Done (ESC)")
+      .click();
+  });
 });
